7997529300daa1a34c0949b0605372f849370ee8
Ekaterina Ivanova <ecatherina96@gmail.com>
2021-05-20 18:24:38 +0300

    Update downlowd.sh

--- ECG features/downlowd.sh
+++ ECG features/downlowd.sh
@@ -1,1 +1,8 @@
+mkdir /data                      
+cd /data                       
 wget https://storage.cloud.google.com/physionet-challenge-2020-12-lead-ecg-public/PhysioNetChallenge2020_Training_2.tar.gz
+tar -xzf PhysioNetChallenge2020_Training_2.tar.gz                                
+rm PhysioNetChallenge2020_Training_2.tar.gz
+
+
+ 
